c07fd742f997ba1296376ed0c0a75dd661669c38
Hirano Koudai <hirano.k.ah@cs-mac88.edu.c.titech.ac.jp>
2017-11-02 12:33:26 +0900

    バリデーション

--- app/controllers/TaskManager.java
+++ app/controllers/TaskManager.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import play.*;
+import play.data.validation.*;
 
 import play.mvc.*;
 
@@ -33,40 +34,58 @@ public class TaskManager extends Controller {
 	}
 
 	public static void signIn() {
-		TMUser usr = TMUser.find("name = ?1", params.get("name")).first();
-		String password = params.get("password");
-		byte[] hashedInput = null;
-		try {
-			Charset charset = StandardCharsets.UTF_8;
-			String algorithm = "MD5";
-			MessageDigest md = MessageDigest.getInstance(algorithm);
-			hashedInput = md.digest(password.getBytes(charset));
-		} catch (NoSuchAlgorithmException e) {
-		}
-		Logger.info("name:%s", params.get("name"));
-		Logger.info("pass:%s", password);
-		Logger.info("hash:%s", hashedInput.toString());
-		Logger.info("hash:%s", usr.hashedPassword.toString());
-		if (Arrays.equals(hashedInput, usr.hashedPassword)) {
-			session.put("user", usr.name);
-			list();
+		List<TMUser> usersByName = TMUser.find("name = ?1", params.get("name")).fetch();
+		if (usersByName.size() == 1) {
+			TMUser usr = usersByName.get(0);
+			String password = params.get("password");
+			byte[] hashedInput = null;
+			try {
+				Charset charset = StandardCharsets.UTF_8;
+				String algorithm = "MD5";
+				MessageDigest md = MessageDigest.getInstance(algorithm);
+				hashedInput = md.digest(password.getBytes(charset));
+			} catch (NoSuchAlgorithmException e) {
+			}
+			// Logger.info("name:%s", params.get("name"));
+			// Logger.info("pass:%s", password);
+			// Logger.info("hash:%s", hashedInput.toString());
+			// Logger.info("hash:%s", usr.hashedPassword.toString());
+			if (Arrays.equals(hashedInput, usr.hashedPassword)) {
+				session.put("user", usr.name);
+				list();
+			} else {
+				signInForm();
+			}
 		} else {
+			validation.current().addError("field", "this account name does not exist", "variables1", "variables2");
+			params.flash();
+			validation.keep();
 			signInForm();
 		}
 	}
 
 	public static void signUp() {
-		Logger.info("name:%s", session.get("name"));
-		Logger.info("pass:%s", session.get("password"));
+		// Logger.info("name:%s", session.get("name"));
+		// Logger.info("pass:%s", session.get("password"));
 		String name = session.get("name");
 		String password = session.get("password");
 		TMUser usr = new TMUser(name, password);
 		usr.save();
-		signInForm();
+		session.put("user", usr.name);
+		list();
 	}
 
 	public static void signUpConfirm() {
-		// validation.required(params.get("name"));
+
+		validation.required("name", params.get("name"));
+		if (!TMUser.find("name = ?1", params.get("name")).fetch().isEmpty()) {
+			validation.current().addError("field", "this account name is already being used", "variables1", "variables2");
+		}
+		if (validation.hasErrors()) {
+			params.flash(); // add http parameters to the flash scope
+			validation.keep(); // keep the errors for the next request
+			signUpForm();
+		}
 		session.put("name", params.get("name"));
 		session.put("password", params.get("password"));
 		render();
@@ -77,6 +96,7 @@ public class TaskManager extends Controller {
 		renderArgs.put("entries", tasks);
 		render();
 	}
+
 	public static void addTask() {
 		Task task = new Task(session.get("user"), params.get("taskName"), params.get("comment"));
 		task.save();
